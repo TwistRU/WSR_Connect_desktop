11becc1862e4fa15acbdf382a3fd0a523ebe25d7
Stellar4 <andrey951768@gmail.com>
2022-02-02 16:08:58 +1000

    вынес переключения между экранами в mainContainer в функции, убрал лишний borderpane из mainScreen

--- src/main/kotlin/ru/wsr/wsr_connect/mainContainerController.kt
+++ src/main/kotlin/ru/wsr/wsr_connect/mainContainerController.kt
@@ -5,7 +5,6 @@ import java.util.ResourceBundle
 import javafx.fxml.FXML
 import javafx.fxml.FXMLLoader
 import javafx.scene.layout.BorderPane
-import ru.wsr.wsr_connect.chatComponents.ChatScreenController
 import ru.wsr.wsr_connect.signComponents.LoginScreenController
 import ru.wsr.wsr_connect.signComponents.SignupScreenController
 
@@ -17,27 +16,29 @@ class MainContainer : BorderPane() {
     @FXML
     private lateinit var location: URL
 
+
+    var login_screen: LoginScreenController? = null
+    var signup_screen: SignupScreenController? = null
+    var main_screen: MainScreenController? = null
+
     @FXML
     fun initialize() {
 
-        val login_screen = LoginScreenController()
-        val signup_screen = SignupScreenController()
-        val main_screen = MainScreenController()
+        login_screen = LoginScreenController()
+        signup_screen = SignupScreenController()
 
 
-        this.center = login_screen
 
-        login_screen.signInButton.setOnAction { e -> this.center = main_screen }
-        login_screen.signUpButton.setOnAction { e -> this.center = signup_screen }
-        signup_screen.backButton.setOnAction { e -> this.center = login_screen }
-        main_screen.avatarImage.setOnMouseClicked { e ->
-            this.center = login_screen
-            main_screen.center = null
-        }
+        switch_to_login_screen()
 
 
+
+        login_screen!!.signInButton.setOnAction { e -> authorise()}
+        login_screen!!.signUpButton.setOnAction { e -> switch_to_signup_screen() }
+        signup_screen!!.backButton.setOnAction { e -> switch_to_login_screen() }
 
     }
+
 
     init {
         val fxmlLoader = FXMLLoader(javaClass.getResource("mainContainer.fxml"))
@@ -45,6 +46,41 @@ class MainContainer : BorderPane() {
         fxmlLoader.setController(this)
         fxmlLoader.load<Any>()
     }
+
+
+
+    fun create_screens(){
+        main_screen = MainScreenController()
+    }
+
+    fun purge_data(){
+        main_screen = null
+    }
+
+    fun switch_to_login_screen(){
+        this.center = login_screen
+    }
+
+    fun switch_to_signup_screen(){
+        this.center = signup_screen
+    }
+
+    fun authorise(){
+        create_screens()
+        this.center = main_screen
+        main_screen!!.avatarImage.setOnMouseClicked { e ->
+            this.center = login_screen
+            main_screen!!.center = null
+        }
+    }
+
+    fun register(){}
+
+    fun logout(){
+        purge_data()
+        switch_to_login_screen()
+    }
+
 
 
 
